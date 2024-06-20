52115c261b4b9ffa954b50cfadcc23a4e9fec95a
uuiid <957714080@qq.com>
2024-06-20 09:11:43 +0800

    fix: 调整一致

---
+++ src/config.ts
@@ -0,0 +1,21 @@
+import categories from './categories';
+import globalGroups from './globalGroups';
+import apps from './rawApps';
+import type { RawSubscription } from './types';
+
+const subsConfig: RawSubscription = {
+  id: 0,
+  version: 0,
+  name: '默认订阅',
+  author: 'lisonge',
+  supportUri: 'https://github.com/gkd-kit/subscription',
+  updateUrl:
+    'https://registry.npmmirror.com/@gkd-kit/subscription/latest/files',
+  checkUpdateUrl:
+    'https://registry.npmmirror.com/@gkd-kit/subscription/latest/files/dist/gkd.version.json',
+  globalGroups,
+  categories,
+  apps,
+};
+
+export default subsConfig;
