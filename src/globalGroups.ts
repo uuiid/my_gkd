7536508b7c3ce8888f533a09fe8e13d9f974842d
uuiid <957714080@qq.com>
2025-02-11 14:50:00 +0800

    fix: 修复一些全局规则

--- src/globalGroups.ts
+++ src/globalGroups.ts
@@ -1,56 +1,109 @@
 import { defineGkdGlobalGroups } from '@gkd-kit/define';
 
-import { batchImportApps } from '@gkd-kit/tools';
+import * as appList from './globalDefaultApps';
+
 const OPEN_AD_ORDER = -10;
-const _app = await batchImportApps(`${import.meta.dirname}/apps`);
-const diabledAppIds = [
-  'com.android.systemui', // 通知栏界面
-  'com.android.settings', // 系统设置
-
-  'com.miui.aod', // miui 锁屏界面
-  'miui.systemui.plugin', // miui 状态栏界面
-
-  // 在一些常见的应用中禁用
-  'com.tencent.mm', // 微信
-  'li.songe.gkd',
-
-  // https://github.com/gkd-kit/gkd/issues/451
-  'mark.via',
-  'com.mycompany.app.soulbrowser',
-  'com.mmbox.xbrowser',
-];
-
-// 如果应用规则已有开屏广告一类的规则, 则在全局规则禁用此应用
-diabledAppIds.push(
-  ..._app
-    .filter((a) =>
-      a.groups.some((g) => g.name.startsWith('开屏广告') && g.enable !== false),
-    )
-    .map((a) => a.id),
-);
+export const UPDATE_PROMPT_ORDER = -9; // 更新提示
+export const YOUTH_MODE_ORDER = -8; // 青少年模式
 
 export default defineGkdGlobalGroups([
   {
     key: 0,
-    name: '开屏广告',
+    name: '开屏广告-全局',
+    desc: '关闭打开应用时的开屏广告',
     order: OPEN_AD_ORDER,
-    actionMaximum: 2,
+    fastQuery: true,
     matchTime: 10000,
+    actionMaximum: 2,
     resetMatch: 'app',
     actionCdKey: 0,
     actionMaximumKey: 0,
+    priorityTime: 10000,
     rules: [
       {
         key: 0,
-        quickFind: true,
+        // 防止在应用的搜索页面误触
+        excludeMatches:
+          '[text*="搜索" || text^="猜你" || text="历史记录" || text$="在搜"][text.length>3 && text.length<6][visibleToUser=true]',
         matches: '[text*="跳过"][text.length<10][visibleToUser=true]',
       },
       {
-        key: -1,
+        key: 1,
+        // 防止在应用的搜索页面误触
+        excludeMatches:
+          '[text*="搜索" || text^="猜你" || text="历史记录" || text$="在搜"][text.length>3 && text.length<6][visibleToUser=true]',
         matches:
-          '[childCount=0][visibleToUser=true][(text.length<10&&(text*="跳过"||text*="跳過"||text*="skip"||text*="Skip")) || id$="tt_splash_skip_btn" || vid*="skip" || vid*="Skip" || (vid*="count" && vid*="down" && vid!*="download") || desc*="跳过" || desc*="skip"]',
+          '[childCount=0][visibleToUser=true][(text.length<10 && (text*="跳过" || text*="跳過" || text~="(?is).*skip.*")) || (vid~="(?is).*skip.*" && text!="帮助" && text!="取消") || id$="tt_splash_skip_btn" || (desc.length<10 && (desc*="跳过" || desc*="跳過" || desc~="(?is).*skip.*"))]',
+        excludeSnapshotUrls: [
+          // 避免误触
+          'https://i.gkd.li/i/17108010', // text!="帮助"
+          'https://i.gkd.li/i/18265000', // text!="取消"
+        ],
+      },
+      {
+        key: 2,
+        action: 'clickCenter',
+        matches:
+          'FrameLayout > FrameLayout[childCount>2] > @View[clickable=true][text=null][visibleToUser=true] + TextView[visibleToUser=true][text=null][index=parent.childCount.minus(1)]', // 字节SDK
       },
     ],
-    apps: diabledAppIds.map((id) => ({ id, enable: false })),
+    apps: [...appList.openAdBlackListAppIDs]
+      .map((id) => ({ id, enable: false }))
+      .concat(
+        [...appList.openAdWhiteListAppIDs].map((id) => ({ id, enable: true })),
+      ),
+  },
+  {
+    key: 1,
+    name: '更新提示-全局',
+    desc: '关闭应用的更新弹窗',
+    order: UPDATE_PROMPT_ORDER,
+    fastQuery: true,
+    matchTime: 10000,
+    actionMaximum: 1,
+    resetMatch: 'app',
+    rules: [
+      {
+        key: 0,
+        matches: [
+          '[text*="内测" || text*="测试版" || text*="新版" || text*="更新" || text*="升级" || text*="体验" || text*="內測" || text*="測試版" || text*="升級" || text*="體驗" || text*="Update" || text*="Upgrade" || text*="Experience"][text!*="自动" && text!*="自動" && text!*="成功" && text!*="失败" && text!*="失敗" && text!*="检查更新" && text!*="检测更新" && text!*="卸载"][name!$=".CheckBox"][childCount=0][visibleToUser=true]',
+          '[text*="更新" || text*="下载" || text*="安装" || text*="升级" || text*="查看" || text*="体验" || text*="确定" || text*="确认"][text.length<6][name!$=".CheckBox"][childCount=0][visibleToUser=true]',
+          '[text*="不再提醒" || text$="再说" || text$="拒绝" || text$="再想想" || text*="再看看" || text^="忽略" || text^="暂不" || text^="放弃" || text^="取消" || text$="不要" || text$="再說" || text$="暫不" || text$="拒絕" || text*="稍后" || text^="关闭" || text$="Later" || text^="Ignore" || text^="Not now" || text^="Cancel" || vid="iv_close" || vid="iv_cancel" || vid="close" || vid="Close" || vid="img_close" || vid="btn_close" || vid="ivCancel" || vid="tvCancel" || vid="cancel" || vid="Cancel" || vid="ivClose" || vid="imgClose" || vid="iv_negative"][name!$=".CheckBox"][childCount=0][visibleToUser=true]',
+        ],
+        excludeSnapshotUrls: [
+          // 避免误触
+          'https://i.gkd.li/i/17710149', // text!*="卸载"
+        ],
+      },
+    ],
+    apps: [...appList.updateBlackListAppIDs]
+      .map((id) => ({ id, enable: false }))
+      .concat(
+        [...appList.updateWhiteListAppIDs].map((id) => ({ id, enable: true })),
+      ),
+  },
+  {
+    key: 2,
+    name: '青少年模式-全局',
+    desc: '关闭应用的青少年模式弹窗',
+    order: YOUTH_MODE_ORDER,
+    fastQuery: true,
+    matchTime: 10000,
+    actionMaximum: 1,
+    resetMatch: 'app',
+    rules: [
+      {
+        key: 0,
+        matches: [
+          '[text*="青少年" || text*="未成年" || text*="儿童"][text*="模式" || text*="守护"][text.length<15][childCount=0][visibleToUser=true]',
+          '[text*="知道了" || text*="关闭" || text*="我已知晓" || text*="已满"][text.length<8][childCount=0][visibleToUser=true]',
+        ],
+      },
+    ],
+    apps: [...appList.yongBlackListAppIDs]
+      .map((id) => ({ id, enable: false }))
+      .concat(
+        [...appList.yongWhiteListAppIDs].map((id) => ({ id, enable: true })),
+      ),
   },
 ]);
